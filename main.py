a42aa854dc4b23240ee5154e27d1ec69bc968a79
Lawson Higgins <lawsoncadehiggins040409@gmail.com>
2025-02-15 21:00:47 -0700

    final_commit4

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-import yt_dlp, os
-def download_mp3(url):
-    ydl_opts = {
-        'format': 'bestaudio/best',  # Best audio format
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',  # Use FFmpegExtractAudio instead of FFmpegAudioConvertor
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'outtmpl': os.path.join(os.getcwd()+'/downloads', '%(title)s.%(ext)s'),  # Save the file with the video title
-    }
+import yt_dlp, os, sys
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        try:
-            print(f"Downloading {url}...")
-            ydl.download([url])
-            print("Download complete!")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+while True:
+    script_dir = os.path.dirname(sys.executable)  # Get script directory
+    parent_dir = os.path.dirname(script_dir)
 
-if __name__ == "__main__":
-    url = input("url: ")
-    download_mp3(url)
-    
-    
+
+    def download_mp3(url):
+        ydl_opts = {
+            'format': 'bestaudio/best',  # Best audio format
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',  # Use FFmpegExtractAudio instead of FFmpegAudioConvertor
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'outtmpl': os.path.join(parent_dir+'/downloads', '%(title)s.%(ext)s'),  # Save the file with the video title
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            try:
+                print(f"Downloading {url}...")
+                ydl.download([url])
+                print("Download complete!")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+    if __name__ == "__main__":
+        print("--------------------------------")
+        url = input("Enter The URL: ")
+        print("--------------------------------")
+        download_mp3(url)
+        
+        
